e8777256ecd62b30992695f6d878eee1bd3dd173
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-04-19 11:49:59 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gastronomish`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
